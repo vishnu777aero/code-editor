029fac2488b0ba8faa2556791a2bf1d220f3b014
vishnu777aero <vishnu777aero@gmail.com>
2021-05-28 23:32:28 +0530

    remove hardcoded values

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -3,19 +3,19 @@ import "codemirror/theme/material.css";
 
 import { Controlled as ControlledEditor } from "react-codemirror2";
 
-export const Editor = () => {
+export const Editor = ({ value, onBeforeChange, className, mode }) => {
+  const options = {
+    mode,
+    theme: "material",
+    lineNumbers: true,
+  };
+
   return (
     <ControlledEditor
-      value="<h1>I ♥ react-codemirror2</h1>"
-      options={{
-        mode: "xml",
-        theme: "material",
-        lineNumbers: true,
-      }}
-      onBeforeChange={(editor, data, value) => {
-        console.log(editor, data, value);
-      }}
-      className="editor"
+      value={value}
+      options={options}
+      className={className}
+      onBeforeChange={onBeforeChange}
     />
   );
 };
