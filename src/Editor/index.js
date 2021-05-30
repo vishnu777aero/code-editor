d5935994a5329d8b2beb2d79391bdb9246beac79
vishnu777aero <vishnu777aero@gmail.com>
2021-05-30 15:06:53 +0530

    add resize icons

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -9,6 +9,8 @@ import { useState } from "react";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Editor = ({ value, onBeforeChange, className, mode, name }) => {
   const options = {
@@ -18,19 +20,28 @@ export const Editor = ({ value, onBeforeChange, className, mode, name }) => {
   };
 
   const editorRef = useRef();
-  const [resizeButtonText, setResizeButtonText] = useState("collapse");
+  const [resizeState, setResizeState] = useState("collapse");
 
   const onResize = (event) => {
     editorRef.current.classList.toggle("collapse");
-    const buttonText =
-      event.target.innerHTML === "collapse" ? "expand" : "collapse";
-    setResizeButtonText(buttonText);
+    setResizeState((prevState) =>
+      prevState === "collapse" ? "expand" : "collapse"
+    );
   };
+
+  const resizeButtonIcon =
+    resizeState === "collapse" ? (
+      <FontAwesomeIcon icon={faCompressAlt} />
+    ) : (
+      <FontAwesomeIcon icon={faExpandAlt} />
+    );
 
   const header = (
     <div className="header">
       <span>{name}</span>
-      <button onClick={onResize}>{resizeButtonText}</button>
+      <div className="resize-button" onClick={onResize}>
+        {resizeButtonIcon}
+      </div>
     </div>
   );
 
